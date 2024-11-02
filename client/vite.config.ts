abe066c7fe8324bac2995941a53b8ba0615a451a
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2024-11-03 01:15:24 +0300

    (client) edit vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
 })
